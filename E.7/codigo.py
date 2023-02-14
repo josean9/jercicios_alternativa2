6375a6cd55ec097e3b12f43a2e59861b8cc66474
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 17:15:24 +0100

    costo trayecto

---
+++ E.7/codigo.py
@@ -0,0 +1,11 @@
+# viaje escolar 
+print("Calculo de viaje escolar segun el numero de alumnos y el costo por alumno")
+alumnos = int(input("Ingrese el numero de alumnos: "))
+dias = int(input("Ingrese el numero de dias: "))
+def trayecto(alumnos):
+    if alumnos <= 25:
+        costotrayecto = 67,30*alumnos
+    elif alumnos > 25 :
+        costotrayecto = 61*alumnos
+    return costotrayecto
+print("El costo total del trayecto es de: ", trayecto(alumnos), "€")
