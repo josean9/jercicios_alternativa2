4f95b1593e5b9d0a12aed9fdb76009234cd44f16
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 17:09:06 +0100

    descuentos segun numero proc

--- E.6/codigo.py
+++ E.6/codigo.py
@@ -7,3 +7,14 @@ def descuentos():
     if abonado == "S":
         print("A cual de los 2? (COMMAQ/BEL)")
         abonado = input()
+    print("Cuantos microprocesadores quieres comprar?")
+    cantidad = input(int())
+    if cantidad (10000, 20000):
+        if abonado == "COMMAQ":
+            descuento2 = 8/100
+            print("Va a tener un descuento del 8%")
+        elif abonado == "BEL":
+            descuento2 = 11/100
+            print("Va a tener un descuento del 11%")
+        else:
+            print("Va a tener un descuento del 10%")
