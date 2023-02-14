5de44aed38ad793d55f64a2d621528e42b6b3008
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 16:55:35 +0100

    seleccion dos numeros y operar

---
+++ E.2/codigo.py
@@ -0,0 +1,7 @@
+#Números, suma y producto
+print("Elige dos numeros")
+a = int(input("a: "))
+b = int(input("b: "))
+#Ahora vamos a ver el resultado de la suma y el producto
+suma = a + b
+producto = a * b
