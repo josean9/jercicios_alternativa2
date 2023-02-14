d07ac88ae9f6dabe89cdb448962187429db607b2
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 17:03:43 +0100

    metodo sort

--- E.2/codigo.py
+++ E.2/codigo.py
@@ -5,22 +5,10 @@ b = int(input("b: "))
 #Ahora vamos a ver el resultado de la suma y el producto
 suma = a + b
 producto = a * b
-#decoradores
-def funcion_decoradora1(funcion_parametro1):
-    def funcion_interior1():
-        print("Vamos a hacer la suma: ")
-        funcion_parametro1()
-        print("Hemos terminado la suma")
-    return funcion_interior1
-def funcion_decoradora(funcion_parametro):
-    def funcion_interior():
-        print("Vamos a hacer la multiplicacion: ")
-        funcion_parametro()
-        print("Hemos terminado la multiplicacion")
-    return funcion_interior
-@funcion_decoradora1
-def suma():
-    print(suma)
-@funcion_decoradora
-def producto():
-    print(producto)
+print("La suma de los dos numeros es: ", suma)
+print("El producto de los dos numeros es: ", producto)
+#compararemos los numero a continuacion
+lista = [a, b, suma, producto]
+lista.sort()
+print("asi quedaria ordenados de menor a mayor ", lista)
+
