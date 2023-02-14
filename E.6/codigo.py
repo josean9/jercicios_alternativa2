92a15ad9adb19c6adc0f494fb53f028ad6f9ae36
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 17:08:15 +0100

    descuentos y abonados

---
+++ E.6/codigo.py
@@ -0,0 +1,9 @@
+#Ejercicio 6: Descuento en los microprocesadores
+
+def descuentos():
+    print("A continuacion tendra una calculadora de descuentos en los microprocesadores")
+    print("Eres abonado a COMMAQ o a BEL? (S/N)")
+    abonado = input()
+    if abonado == "S":
+        print("A cual de los 2? (COMMAQ/BEL)")
+        abonado = input()
