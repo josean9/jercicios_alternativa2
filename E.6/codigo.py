1a26edfb158ec00e2da8e08512f377d47e61dff5
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-02-14 17:12:18 +0100

    ajuste numero 2

--- E.6/codigo.py
+++ E.6/codigo.py
@@ -10,30 +10,25 @@ def descuentos():
     print("Cuantos microprocesadores quieres comprar?")
     cantidad = input(int())
     if cantidad (10000, 20000):
+    
         if abonado == "COMMAQ":
-            descuento2 = 8/100
             print("Va a tener un descuento del 8%")
         elif abonado == "BEL":
-            descuento2 = 11/100
             print("Va a tener un descuento del 11%")
         else:
             print("Va a tener un descuento del 10%")
     elif cantidad (20001, 40000 ):
         if abonado == "COMMAQ":
-            descuento2 = 13/100
             print("Va a tener un descuento del 13%")
         elif abonado == "BEL":
-            descuento2 = 16/100
             print("Va a tener un descuento del 16%")
         else:
             print("Va a tener un descuento del 15%")
     
     elif cantidad > 40000:
         if abonado == "COMMAQ":
-            descuento2 = 18/100
             print("Va a tener un descuento del 18%")
         elif abonado == "BEL":
-            descuento2 = 21/100
             print("Va a tener un descuento del 21%")
         else:
             print("Va a tener un descuento del 20%")
